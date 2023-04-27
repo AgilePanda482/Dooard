2332927b9f9fdc6591d42ce793ddbf28bde04fe3
David Vela <dsrv06@gmail.com>
2023-04-26 22:25:23 -0600

    Modificación: Agregando compatibilidad ESP32

--- DOARD.ino
+++ DOARD.ino
@@ -1,7 +1,7 @@
 //Libreria para la comuniciacion del ESP32 y Telegram
 #include <CTBot.h>
 //Informacion sencible de conexiones a internet y Telegram
-#include <//home/david/Documentos/ProgramasDeArduino/DOARD/info.h>
+#include </home/david/Documentos/ProgramasDeArduino/DOARD/librerias/token.h>
 
 /*Plantilla que permite imprimir informacion como en C++ sin recurrir al ciclo FOR
 Ejemplo: Serial << "Hola mundo!"*/
@@ -10,9 +10,30 @@ template<class T> inline Print &operator <<(Print &obj, T arg) {
     return obj;
 }
 
+//Modelo de la camara
+#define CAMERA_MODEL_AI_THINKER
+
+//Pines para controlar la camara
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
 CTBot DOARD;
-const byte botonPin = 35;
-const byte buzzPin = 33;
+const byte botonPin = 14;
+const byte buzzPin = 13;
 byte leerBoton;
 const byte dt = 50;
 
@@ -20,14 +41,13 @@ void setup(){
     Serial.begin(115200);
     Serial.println("Iniciando...");
 
-
     pinMode(botonPin, INPUT);
     pinMode(buzzPin, OUTPUT);
     
     //Conexion a internet y a telegram
     DOARD.wifiConnect(ssid, password);
     DOARD.setTelegramToken(token);
-
+    
     //Detecta si todo a salido bien. En caso de 15 intentos fallidos de conexion, informara del error
     if(DOARD.testConnection() == true){
         Serial.println("\n Conectado Correctamente");
